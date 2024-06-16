c35ec3a85f280934c915160ab133116ca776374d
jeffreyai7 <jirukeh@gmail.com>
2024-06-16 18:12:42 +0100

    fix: image path

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,9 +15,9 @@ function Home({email, handleChange, handleSubmit, emailerr, erralert, handleFocu
     <div className="secondflexbox">
       <h1>Stay Updated</h1>
       <p>Join 60,000+ product managers receiving monthly updates on: </p>
-      <div className="list"><img src="assets/images/icon-list.svg" alt="list"/><span>Product discovery and building what matters</span></div>
-      <div className="list"><img src="assets/images/icon-list.svg" alt="list"/><span> Measuring to ensure updates are a success</span></div>
-      <div className="list"><img src="assets/images/icon-list.svg" alt="list"/><span> And much more</span></div>
+      <div className="list"><img src="/assets/images/icon-list.svg" alt="list"/><span>Product discovery and building what matters</span></div>
+      <div className="list"><img src="/assets/images/icon-list.svg" alt="list"/><span> Measuring to ensure updates are a success</span></div>
+      <div className="list"><img src="/assets/images/icon-list.svg" alt="list"/><span> And much more</span></div>
       <div className="formInput">
       <div>
       <div className="err-container"><label htmlFor="email"><small><b>Email address</b></small></label> <small className="errmsg">{emailerr}</small></div>
