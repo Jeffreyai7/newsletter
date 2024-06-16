6b66024acd6160d1bafce816b518591d76b1c37a
jeffreyai7 <jirukeh@gmail.com>
2024-06-16 18:36:43 +0100

    fix: path and color

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "./home.css"
+import listimage from "/assets/images/icon-list.svg"
+import desktopimage from "/assets/images/illustration-sign-up-desktop.svg"
+import mobileimage from "/assets/images/illustration-sign-up-mobile.svg"
+
 
 
 function Home({email, handleChange, handleSubmit, emailerr, erralert, handleFocus}){
@@ -9,15 +13,15 @@ function Home({email, handleChange, handleSubmit, emailerr, erralert, handleFocu
   <div className="wrapper">
     <div className='container'>
       <picture className="flexbox1">
-      <source media="(min-width: 750px)" srcset="assets/images/illustration-sign-up-desktop.svg"/>
-      <img src="assets/images/illustration-sign-up-mobile.svg" alt="" />
+      <source media="(min-width: 750px)" srcset={desktopimage} />
+      <img src={mobileimage} alt="displaypic" />
       </picture>
     <div className="secondflexbox">
       <h1>Stay Updated</h1>
       <p>Join 60,000+ product managers receiving monthly updates on: </p>
-      <div className="list"><img src="/assets/images/icon-list.svg" alt="list"/><span>Product discovery and building what matters</span></div>
-      <div className="list"><img src="/assets/images/icon-list.svg" alt="list"/><span> Measuring to ensure updates are a success</span></div>
-      <div className="list"><img src="/assets/images/icon-list.svg" alt="list"/><span> And much more</span></div>
+      <div className="list"><img src={listimage} alt="list"/><span>Product discovery and building what matters</span></div>
+      <div className="list"><img src={listimage} alt="list"/><span> Measuring to ensure updates are a success</span></div>
+      <div className="list"><img src={listimage} alt="list"/><span> And much more</span></div>
       <div className="formInput">
       <div>
       <div className="err-container"><label htmlFor="email"><small><b>Email address</b></small></label> <small className="errmsg">{emailerr}</small></div>
